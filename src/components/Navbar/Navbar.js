074b6d02c86ae83e35ce865e023aeeaa13ba0440
shagors <shajjadshagor@gmail.com>
2022-05-01 03:49:37 +0600

    navbar add and routes setup

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+    return (
+        <nav className="relative w-full flex flex-wrap items-center justify-between py-3 bg-gray-100 text-gray-500 hover:text-gray-700 focus:text-gray-700 shadow-lg">
+            <div className="container-fluid w-full flex flex-wrap items-center justify-between md:px-40 px-5">
+                <div className="container-fluid">
+                    <Link className="text-xl text-black" to="/">Logo</Link>
+                </div>
+                <div>
+                    <Link className='md:px-3 px-1' to='/home'>Home</Link>
+                    <Link className='md:px-3 px-1' to='/home'>Orders</Link>
+                    <Link className='md:px-3 px-1' to='/home'>User</Link>
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
