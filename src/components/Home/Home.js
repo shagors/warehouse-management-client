fdfff25540031e2e4a92fbe88bfc723f31a0e5b6
shagors <shajjadshagor@gmail.com>
2022-05-07 12:31:20 +0600

    Our partners extra options add on home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Inventorys from '../../Inventory/Inventorys/Inventorys';
+import OurPartners from '../../OurPartners/OurPartners';
 import OurService from '../../OurService/OurService';
 import Banner from '../Banner/Banner';
 
@@ -7,10 +8,12 @@ const Home = () => {
     return (
         <div>
             <Banner></Banner>
-            <h1 className='mx-40 text-center text-3xl text-emerald-700 my-4'>Our Products List</h1>
+            <h1 className='mx-40 text-center text-3xl text-emerald-700 my-5'>Our Products List</h1>
             <Inventorys></Inventorys>
-            <h1 className='mx-40 text-center text-3xl text-emerald-700 my-4'>Our Services</h1>
+            <h1 className='mx-40 text-center text-3xl text-emerald-700 my-5'>Our Services</h1>
             <OurService></OurService>
+            <h1 className='mx-40 text-center text-3xl text-emerald-700 my-5'>Our Partners</h1>
+            <OurPartners></OurPartners>
         </div>
     );
 };
