3b66e1907eee6dc679f054ffcf8f48b40b8c30b0
shagors <shajjadshagor@gmail.com>
2022-05-08 00:21:11 +0600

    shows products in client side from database

--- src/Inventory/InventoryDetails/InventoryDetails.js
+++ src/Inventory/InventoryDetails/InventoryDetails.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 const InventoryDetails = () => {
     const {inventoryId} = useParams();
+    const [product, setProduct] = useState({});
+
+    useEffect( () => {
+        const url = `http://localhost:5000/products/${inventoryId}`
+        fetch(url)
+        .then(res => res.json())
+        .then(data => setProduct(data))
+    } ,[]);
+
     return (
         <div className='mx-40 my-5'>
-            <h2 className='text-3xl text-center text-green-600'>Inventory Details: {inventoryId}</h2>
+            <h2 className='text-3xl text-center text-green-600'>Inventory Product Details: {product.name}</h2>
         </div>
     );
 };
