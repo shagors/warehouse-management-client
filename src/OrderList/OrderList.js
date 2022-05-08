8532bf6e983be480b2393e5e66e4930ed6d6bc3a
shagors <shajjadshagor@gmail.com>
2022-05-08 19:06:04 +0600

    readme.md file add

---
+++ src/OrderList/OrderList.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import useProducts from '../hooks/useProducts/useProducts';
+
+const OrderList = ({product}) => {
+    const [products, setProducts] = useProducts();
+
+    const handleDelete = (id) => {
+        const proceed = window.confirm('Are You Sure');
+        if(proceed){
+            const url = `http://localhost:5000/products/${id}`;
+            fetch(url, {
+                method: 'DELETE'
+            })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                const remaining = products.filter(product => product._id !== id);
+                setProducts(remaining);
+            })
+        }
+    }
+
+    return (
+        <div className='mx-40'>
+            <div className='my-5' key={product._id}>
+                <h3 className='text-xl text-center'>{product.name}
+                    <button onClick={() => handleDelete(product._id)} type="button" className="ml-20 inline-block px-6 py-2.5 bg-red-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-red-700 hover:shadow-lg focus:bg-red-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-red-800 active:shadow-lg transition duration-150 ease-in-out">Delete
+                    </button>
+                </h3>
+            </div>
+            {/* <div class="flex flex-col">
+                <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
+                    <div class="py-2 inline-block min-w-full sm:px-6 lg:px-8">
+                    <div class="overflow-hidden">
+                        <table class="min-w-full">
+                        <thead class="border-b">
+                            <tr>
+                            <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                                Product Name
+                            </th>
+                            <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                                Product Quantity
+                            </th>
+                            <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                                Delete
+                            </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr class="border-b">
+                            <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                {product.name}
+                            </td>
+                            <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                {product.quantity}
+                            </td>
+                            <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                <button onClick={() => handleDelete(product._id)} type="button" className="ml-20 inline-block px-6 py-2.5 bg-red-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-red-700 hover:shadow-lg focus:bg-red-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-red-800 active:shadow-lg transition duration-150 ease-in-out">Delete
+                                </button>
+                            </td>
+                            </tr>
+                        </tbody>
+                        </table>
+                    </div>
+                    </div>
+                </div>
+            </div> */}
+        </div>
+    );
+};
+
+export default OrderList;
--- src/Orders/Orders.js
+++ src/Orders/Orders.js
@@ -1,78 +1,17 @@
-import { data } from 'autoprefixer';
 import React from 'react';
 import useProducts from '../hooks/useProducts/useProducts';
+import OrderList from '../OrderList/OrderList';
 
 const Orders = () => {
     const [products, setProducts] = useProducts();
 
-    const handleDelete = (id) => {
-        const proceed = window.confirm('Are You Sure');
-        if(proceed){
-            const url = `http://localhost:5000/products/${id}`;
-            fetch(url, {
-                method: 'DELETE'
-            })
-            .then(res => res.json())
-            .then(data => {
-                console.log(data);
-                const remaining = products.filter(product => product._id !== id);
-                setProducts(remaining);
-            })
-        }
-    }
     return (
         <div className='md:mx-40'>
             <h1 className='my-5 text-3xl text-center text-green-500'>Manage products List</h1>
             {
-                products.map(product => 
-                <div className='my-5' key={product._id}>
-                    <h3 className='text-xl text-center'>{product.name}
-                    <button onClick={() => handleDelete(product._id)} type="button" className="ml-20 inline-block px-6 py-2.5 bg-red-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-red-700 hover:shadow-lg focus:bg-red-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-red-800 active:shadow-lg transition duration-150 ease-in-out">Delete</button>
-                    </h3>
-                    {/* <div className="flex flex-col">
-                        <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
-                            <div className="py-2 inline-block min-w-full sm:px-6 lg:px-8">
-                            <div className="overflow-hidden">
-                                <table className="min-w-full">
-                                <thead className="bg-white border-b">
-                                    <tr>
-                                    <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
-                                        #
-                                    </th>
-                                    <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
-                                        First
-                                    </th>
-                                    <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
-                                        Handle
-                                    </th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr className="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100">
-                                    <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">1</td>
-                                    <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                                        Mark
-                                    </td>
-                                    <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                                        Otto
-                                    </td>
-                                    </tr>
-                                    <tr className="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100">
-                                    <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">2</td>
-                                    <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                                        Jacob
-                                    </td>
-                                    <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                                        Thornton
-                                    </td>
-                                    </tr>
-                                </tbody>
-                                </table>
-                            </div>
-                            </div>
-                        </div>
-                    </div> */}
-                </div>)
+                products.map(product => <OrderList
+                key={product._id} product={product}
+                ></OrderList>)
             }
         </div>
     );
